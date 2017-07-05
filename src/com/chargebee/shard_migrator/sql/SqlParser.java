11282309cb3d164ee01cb61a2b12449c46a522d0
vikiuvb <vignesh@chargebee.com>
2017-07-05 16:17:32 +0530

    changes

--- src/com/chargebee/shard_migrator/sql/SqlParser.java
+++ src/com/chargebee/shard_migrator/sql/SqlParser.java
@@ -22,23 +22,19 @@ public class SqlParser {
         }
          
         Set<SqlStmt> statements = new HashSet<>();
-        SqlStmt stmt = new SqlStmt();
-        stmt.step = 1;
-        stmt.sql = script;
-        statements.add(stmt);
-//        Matcher m = convPat.matcher(script);
-//        while (m.find()) {
-//            SqlStmt stmt = new SqlStmt();
-//            stmt.step = Integer.parseInt(m.group(1));
-//            stmt.sql = m.group(2).trim();
-//            if (stmt.sql.endsWith(";")) {
-//                stmt.sql = stmt.sql.substring(0, stmt.sql.length() - 1);
-//            }
-//            if (statements.contains(stmt)) {
-//                throw new RuntimeException("Duplicate statement " + stmt);
-//            }
-//            statements.add(stmt);
-//        }
+        Matcher m = convPat.matcher(script);
+        while (m.find()) {
+            SqlStmt stmt = new SqlStmt();
+            stmt.step = Integer.parseInt(m.group(1));
+            stmt.sql = m.group(2).trim();
+            if (stmt.sql.endsWith(";")) {
+                stmt.sql = stmt.sql.substring(0, stmt.sql.length() - 1);
+            }
+            if (statements.contains(stmt)) {
+                throw new RuntimeException("Duplicate statement " + stmt);
+            }
+            statements.add(stmt);
+        }
         return statements;
     }
 
